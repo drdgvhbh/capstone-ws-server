e8aa536f481c359b112be47448f18fb56cdbc084
Ryan Lee <drdgvhbh@gmail.com>
2019-12-02 22:14:38 -0500

    add server

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import WebSocket from 'ws';
 
 const wss = new WebSocket.Server({ port: 8080 });
 
+const connections: WebSocket[] = [];
+
 wss.on('connection', function connection(ws) {
+  connections.push(ws);
   ws.on('message', function incoming(message) {
     console.log(message);
-    ws.send(message);
+    for (const connection of connections) {
+      connection.send(message);
+    }
   });
 });
